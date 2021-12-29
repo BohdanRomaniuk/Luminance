c6575691755d9f90139d1c486576124e871945ec
BohdanRomaniuk <bohdan2307@gmail.com>
2021-12-29 22:55:45 +0200

    Add more effects

--- SmartChristmasTree/SmartChristmasTree.ino
+++ SmartChristmasTree/SmartChristmasTree.ino
@@ -1,8 +1,9 @@
 #include <FastLED.h>
+#include "gradients.h"
 
 #define LED_PIN     6
 #define NUM_LEDS    60
-#define BRIGHTNESS  50
+#define BRIGHTNESS  255
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 #define VOLTAGE 5
@@ -13,7 +14,7 @@
 #define qsuba(x, b)  ((x>b)?x-b:0)
 
 CRGB leds[NUM_LEDS];
-CRGBPalette16 gCurrentPalette;
+CRGBPalette16 currentPalette;
 TBlendType currentBlending = LINEARBLEND;
 int8_t  currentMode = 0;
 uint32_t effectsTimer = 0;
@@ -25,36 +26,17 @@ uint8_t thishue = 0;
 uint8_t thisrot = 1;
 uint8_t allfreq = 32;
 uint8_t thiscutoff = 192;
-
-//two sin
 uint8_t thathue = 140;
 uint8_t thatrot = 0;
 int8_t thatspeed = 4;
 int thatphase = 0;
 uint8_t thatcutoff = 192;
-//two sin
-
-//rainbow march
 uint8_t thisdiff = 1;
 uint8_t thisindex = 0;
-//rainbow march
-
 uint8_t thisfade = 224;
 uint8_t thisinc = 1;
-
-DEFINE_GRADIENT_PALETTE( Sunset_Real_gp ) {
-  0, 120,  0,  0,
-  22, 179, 22,  0,
-  51, 255, 104,  0,
-  85, 167, 22, 18,
-  135, 100,  0, 103,
-  198,  16,  0, 130,
-  255,   0,  0, 160
-};
-const TProgmemRGBGradientPalettePtr gGradientPalettes[] = {
-  Sunset_Real_gp
-
-};
+uint16_t dist = 12345;
+uint8_t scale = 30;
 
 void setup()
 {
@@ -66,7 +48,7 @@ void setup()
     FastLED.setMaxPowerInVoltsAndMilliamps(VOLTAGE, CURRENT_LIMIT);
   }
 
-  gCurrentPalette = gGradientPalettes[0];
+  currentPalette = RainbowColors_p;
 }
 
 void loop()
@@ -87,98 +69,155 @@ void runEffects()
       blendwave();
       break;
     case 1:
-      rainbow_beat();
+      currentPalette = emeraldDragonPalette;
+      noise8_pal();
       break;
     case 2:
-      allfreq = 2;
-      thisspeed = 1;
-      thatspeed = 1;
-      thishue = 0;
-      thathue = 128;
+      currentPalette = LavaColors_p;
       thisdir = 1;
-      thisrot = 1;
-      thatrot = 1;
-      thiscutoff = 128;
-      thatcutoff = 192;
-
-      two_sin();
+      colorwaves();
       break;
     case 3:
-      allfreq = 4;
-      thisspeed = -1;
-      thatspeed = 0;
-      thishue = 64;
-      thathue = 192;
-      thisdir = 1;
-      thisrot = 0;
-      thatrot = 0;
-      thiscutoff = 64;
-      thatcutoff = 192;
-
-      two_sin();
+      rainbow_beat();
       break;
     case 4:
-      allfreq = 6;
-      thisspeed = 2;
-      thatspeed = 1;
-      thishue = 96;
-      thathue = 224;
-      thisdir = 1;
-      thisrot = 0;
-      thatrot = 0;
-      thiscutoff = 64;
-      thatcutoff = 64;
-
-      two_sin();
+      currentPalette = sunsetPalette;
+      noise8_pal();
       break;
     case 5:
-      allfreq = 20;
-      thisspeed = 2;
-      thatspeed = 0;
-      thishue = 24;
-      thathue = 180;
-      thisdir = 1;
-      thisrot = 0;
-      thatrot = 1;
-      thiscutoff = 64;
-      thatcutoff = 128;
-
-      two_sin();
+      currentPalette = landscapePalette;
+      thisdir = -1;
+      colorwaves();
       break;
     case 6:
-      thisspeed = 2;
-      thatspeed = 3;
-      thishue = 96;
-      thathue = 224;
-      thisdir = 1;
-      thisrot = 1;
-      thatrot = 2;
-      thiscutoff = 128;
-      thatcutoff = 64;
-
+      setup_two_sin(2, 1, 1, 185, 64, 1, 1, 1, 128, 192);
       two_sin();
       break;
     case 7:
+      currentPalette = rgiPalette;
       noise8_pal();
       break;
     case 8:
+      currentPalette = PartyColors_p;
+      thisdir = 1;
+      colorwaves();
+      break;
+    case 9:
+      setup_two_sin(4, -1, 1, 0, 200, 1, 0, 0, 64, 192);
+      two_sin();
+      break;
+    case 10:
+      currentPalette = retroPalette;
+      noise8_pal();
+      break;
+    case 11:
+      currentPalette = blueCyanYellowPalette;
+      thisdir = -1;
+      colorwaves();
+      break;
+    case 12:
+      setup_two_sin(4, 2, 1, 0, 128, 1, 0, 0, 0, 0);
+      two_sin();
+      break;
+    case 13:
+      currentPalette = analogousPalette;
+      noise8_pal();
+      break;
+    case 14:
+      currentPalette = RainbowColors_p;
+      thisdir = 1;
+      colorwaves();
+      break;
+    case 15:
+      setup_two_sin(6, 2, 0, 24, 75, 1, 0, 1, 64, 128);
+      two_sin();
+      break;
+    case 16:
+      currentPalette = pinkPalette;
+      noise8_pal();
+      break;
+    case 17:
+      currentPalette = analogousPalette;
+      thisdir = 1;
+      colorwaves();
+      break;
+    case 18:
+      setup_two_sin(2, 1, 1, 25, 152, 1, 1, 2, 64, 64);
+      two_sin();
+      break;
+    case 19:
+      currentPalette = pinksplashPalette;
+      noise8_pal();
+      break;
+    case 20:
+      currentPalette = hult65Palette;
+      thisdir = -1;
+      colorwaves();
+      break;
+    case 21:
       thisdir = 1;
       thisrot = 1;
       thisdiff = 1;
 
       rainbow_march();
       break;
-    case 9:
+    case 22:
+      thisdir = -1;
+      thisrot = 1;
+      thisdiff = 1;
+
+      rainbow_march();
+      break;
+    case 23:
+      currentPalette = landscapePalette;
+      noise8_pal();
+      break;
+    case 24:
+      currentPalette = emeraldDragonPalette;
+      thisdir = 1;
+      colorwaves();
+      break;
+    case 25:
+      currentPalette = hult65Palette;
+      noise8_pal();
+      break;
+    case 26:
       thisinc = 1;
       thisfade = 2;
       thisdiff = 32;
-
       confetti_pal();
       break;
-    case 10:
+    case 27:
+      currentPalette = blueCyanYellowPalette;
+      noise8_pal();
+      break;
+    case 28:
+      currentPalette = sunsetPalette;
+      thisdir = -1;
+      colorwaves();
+      break;
+    case 29:
+      currentPalette = PartyColors_p;
+      noise8_pal();
+      break;
+    case 30:
       thisdir = -1;
       thisrot = 1;
       thisdiff = 5;
+      rainbow_march();
+    case 31:
+      currentPalette = LavaColors_p;
+      noise8_pal();
+      break;
+    case 32:
+      currentPalette = pinksplashPalette;
+      thisdir = 1;
+      colorwaves();
+      break;
+    case 33:
+      currentPalette = CloudColors_p;
+      noise8_pal();
+      break;
     default:
       currentMode = 0;
       break;
@@ -213,43 +252,48 @@ void blendwave()
 
 void rainbow_beat()
 {
-  if (NUM_LEDS >= 10)
-  {
-    uint8_t startBeat = beatsin8(17, 0, 255);
-    uint8_t endBeat = beatsin8(13, 0, 255);
-    fill_rainbow(leds, NUM_LEDS, (startBeat + endBeat) / 2, 8);
-  }
+  uint8_t startBeat = beatsin8(17, 0, 255);
+  uint8_t endBeat = beatsin8(13, 0, 255);
+  fill_rainbow(leds, NUM_LEDS, (startBeat + endBeat) / 2, 8);
+}
+
+void setup_two_sin(uint8_t freq, int8_t speed1, int8_t speed2, uint8_t hue1, uint8_t hue2, int8_t dir, uint8_t rot1, uint8_t rot2, uint8_t cutoff1, uint8_t cuttoff2)
+{
+  allfreq = freq;
+  thisspeed = speed1;
+  thatspeed = speed2;
+  thishue = hue1;
+  thathue = hue2;
+  thisdir = dir;
+  thisrot = rot1;
+  thatrot = rot2;
+  thiscutoff = cutoff1;
+  thatcutoff = cutoff1;
 }
 
 void two_sin()
 {
-  if (NUM_LEDS >= 10)
-  {
-    thisphase += thisspeed * thisdir;
-    thatphase += thatspeed * thisdir;
+  thisphase += thisspeed * thisdir;
+  thatphase += thatspeed * thisdir;
 
-    thishue = thishue + thisrot;
-    thathue = thathue + thatrot;
+  thishue = thishue + thisrot;
+  thathue = thathue + thatrot;
 
 #if NUM_LEDS < 255
-    uint8_t k;
+  uint8_t k;
 #else
-    uint16_t k;
+  uint16_t k;
 #endif
 
-    for (k = 0; k < NUM_LEDS; ++k)
-    {
-      int thisbright = qsuba(cubicwave8((k * allfreq) + thisphase), thiscutoff);
-      int thatbright = qsuba(cubicwave8((k * allfreq) + 128 + thatphase), thatcutoff);
+  for (k = 0; k < NUM_LEDS; ++k)
+  {
+    int thisbright = qsuba(cubicwave8((k * allfreq) + thisphase), thiscutoff);
+    int thatbright = qsuba(cubicwave8((k * allfreq) + 128 + thatphase), thatcutoff);
 
-      leds[k] = ColorFromPalette(gCurrentPalette, thishue, thisbright, currentBlending);
-      leds[k] += ColorFromPalette(gCurrentPalette, thathue, thatbright, currentBlending);
-    }
+    leds[k] = ColorFromPalette(currentPalette, thishue, thisbright, currentBlending);
+    leds[k] += ColorFromPalette(currentPalette, thathue, thatbright, currentBlending);
   }
 }
-
-uint16_t dist = 12345;
-uint8_t scale = 30;
 
 void noise8_pal()
 {
@@ -258,15 +302,12 @@ void noise8_pal()
 #else
   uint16_t i;
 #endif
-  if (NUM_LEDS >= 10)
+  for ( i = 0; i < NUM_LEDS; i++)
   {
-    for ( i = 0; i < NUM_LEDS; i++)
-    {
-      uint8_t index = inoise8(i * scale, dist + i * scale) % 255;
-      leds[i] = ColorFromPalette(gCurrentPalette, index, 255, currentBlending);
-    }
-    dist += beatsin8(10, 1, 4) * thisdir;
+    uint8_t index = inoise8(i * scale, dist + i * scale) % 255;
+    leds[i] = ColorFromPalette(currentPalette, index, 255, currentBlending);
   }
+  dist += beatsin8(10, 1, 4) * thisdir;
 }
 
 void rainbow_march()
@@ -283,6 +324,67 @@ void confetti_pal()
 #else
   uint16_t pos = random16(NUM_LEDS);
 #endif
-  leds[pos] = ColorFromPalette(gCurrentPalette, thisindex + random8(thisdiff) / 4 , 255, currentBlending);
+  leds[pos] = ColorFromPalette(currentPalette, thisindex + random8(thisdiff) / 4 , 255, currentBlending);
   thisindex = thisindex + thisinc;
+}
+
+void colorwaves()
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+#if NUM_LEDS < 255
+  uint8_t i;
+#else
+  uint16_t i;
+#endif
+
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;
+  uint16_t hueinc16 = beatsin88(113, 300, 1500);
+
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5, 9);
+  uint16_t brightnesstheta16 = sPseudotime;
+
+  for (i = 0 ; i < NUM_LEDS; ++i)
+  {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+    uint16_t h16_128 = hue16 >> 7;
+    if (h16_128 & 0x100)
+    {
+      hue8 = 255 - (h16_128 >> 1);
+    }
+    else
+    {
+      hue8 = h16_128 >> 1;
+    }
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+
+    uint8_t index = hue8;
+    index = scale8(index, 240);
+
+    CRGB newcolor = ColorFromPalette(currentPalette, index, bri8);
+    if (thisdir == 1)
+    {
+      nblend(leds[(NUM_LEDS - 1) - i], newcolor, 128);
+    }
+    else
+    {
+      nblend(leds[i], newcolor, 128);
+    }
+  }
 }
---
+++ SmartChristmasTree/gradients.h
@@ -0,0 +1,111 @@
+//sunsetPalette rgiPalette retroPalette +analogousPalette pinkPalette +pinksplashPalette +landscapePalette +hult65Palette hult64Palette emeraldDragonPalette +blueCyanYellowPalette
+//+RainbowColors_p +PartyColors_p CloudColors_p +LavaColors_p
+DEFINE_GRADIENT_PALETTE(sunsetPalette)
+{
+  0, 120,  0,  0,
+  22, 179, 22,  0,
+  51, 255, 104,  0,
+  85, 167, 22, 18,
+  135, 100,  0, 103,
+  198,  16,  0, 130,
+  255,   0,  0, 160
+};
+
+DEFINE_GRADIENT_PALETTE(rgiPalette)
+{
+  0,   4,  1, 31,
+  31,  55,  1, 16,
+  63, 197,  3,  7,
+  95,  59,  2, 17,
+  127,   6,  2, 34,
+  159,  39,  6, 33,
+  191, 112, 13, 32,
+  223,  56,  9, 35,
+  255,  22,  6, 38
+};
+
+DEFINE_GRADIENT_PALETTE(retroPalette)
+{
+  0, 188, 135,  1,
+  255,  46,  7,  1
+};
+
+DEFINE_GRADIENT_PALETTE(analogousPalette)
+{
+  0,   3,  0, 255,
+  63,  23,  0, 255,
+  127,  67,  0, 255,
+  191, 142,  0, 45,
+  255, 255,  0,  0
+};
+
+DEFINE_GRADIENT_PALETTE(pinkPalette)
+{
+  0, 126, 11, 255,
+  127, 197,  1, 22,
+  175, 210, 157, 172,
+  221, 157,  3, 112,
+  255, 157,  3, 112
+};
+
+DEFINE_GRADIENT_PALETTE(pinksplashPalette)
+{
+  0, 229,  1,  1,
+  61, 242,  4, 63,
+  101, 255, 12, 255,
+  127, 249, 81, 252,
+  153, 255, 11, 235,
+  193, 244,  5, 68,
+  255, 232,  1,  5
+};
+
+DEFINE_GRADIENT_PALETTE(landscapePalette)
+{
+  0,   0,  0,  0,
+  37,   2, 25,  1,
+  76,  15, 115,  5,
+  127,  79, 213,  1,
+  128, 126, 211, 47,
+  130, 188, 209, 247,
+  153, 144, 182, 205,
+  204,  59, 117, 250,
+  255,   1, 37, 192
+};
+
+DEFINE_GRADIENT_PALETTE(hult65Palette) {
+  0, 247, 176, 247,
+  48, 255, 136, 255,
+  89, 220, 29, 226,
+  160,   7, 82, 178,
+  216,   1, 124, 109,
+  255,   1, 124, 109
+};
+
+DEFINE_GRADIENT_PALETTE(hult64Palette)
+{
+  0,   1, 124, 109,
+  66,   1, 93, 79,
+  104,  52, 65,  1,
+  130, 115, 127,  1,
+  150,  52, 65,  1,
+  201,   1, 86, 72,
+  239,   0, 55, 45,
+  255,   0, 55, 45
+};
+
+DEFINE_GRADIENT_PALETTE(emeraldDragonPalette)
+{
+  0,  97, 255,  1,
+  101,  47, 133,  1,
+  178,  13, 43,  1,
+  255,   2, 10,  1
+};
+
+DEFINE_GRADIENT_PALETTE(blueCyanYellowPalette)
+{
+  0,   0,  0, 255,
+  63,   0, 55, 255,
+  127,   0, 255, 255,
+  191,  42, 255, 45,
+  255, 255, 255,  0
+};
